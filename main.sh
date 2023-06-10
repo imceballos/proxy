33b561c1a3b30129480e355e4c167f8a055a3ba7
imceballos <48004620+imceballos@users.noreply.github.com>
2023-06-10 05:19:49 +0000

    Functions

--- main.sh
+++ main.sh
@@ -1,4 +1,34 @@
-sudo apt update -y
-sudo apt -y install squid
-sudo systemctl start squid
-sudo systemctl enable squid
+#!/usr/bin/env bash
+
+function install_dependencies(){
+    sudo apt update -y
+    sudo apt -y install squid
+    }
+
+function start_squid(){
+    sudo systemctl start squid
+    sudo systemctl enable squid
+}
+
+function check_status_netstat(){
+    while true; do
+    port_state=$(sudo netstat -tlnp | grep ":3128" | awk '{print $6}')
+
+    if [[ $port_state == "LISTEN" ]]; then
+        echo "Port 3128 is listening."
+    else
+        echo "Port 3128 is not listening."
+    fi
+
+    sleep 0.1
+done
+}
+
+function uncomment_line(){
+    sed -i '/http_access/s/^# //' /etc/squid/squid.conf
+}
+
+function add_acl(){
+    local ip="$1"
+    acl localnet src "$ip"
+}
